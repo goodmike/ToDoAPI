ed1f276bed650c28e6bd5db66e5df96add8d19dd
Michael Harrison <mh@michaelharrison.ws>
2014-08-29 15:47:50 -0500

    First minitest

---
+++ test/test_get_todos.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "./api"
+require "rack/test"
+
+class TestGetTodos < Minitest::Test
+    include Rack::Test::Methods
+
+    def app
+        Sinatra::Application
+    end
+
+    def test_endpoint_ok
+        get '/todos'
+        assert last_response.ok?
+    end
+end
